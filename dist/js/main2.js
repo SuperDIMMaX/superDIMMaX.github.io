7bf114509496200da088bb608d07c214eb1ea265
SuperDIMMaX <superDIMMaX@mail.ru>
2021-10-19 23:22:09 +0300

    Add files via upload

--- dist/js/main2.js
+++ dist/js/main2.js
@@ -24,3 +24,28 @@ function mobileMenu() {
     x.style.display = "table-row";
   }
 } 
+
+
+var span = document.querySelectorAll('.NameHighlights');
+for (var i = span.length; i--;) {
+    (function () {
+        var t;
+        span[i].onmouseover = function () {
+            hideAll();
+            clearTimeout(t);
+            this.className = 'NameHighlightsHover';
+        };
+        span[i].onmouseout = function () {
+            var self = this;
+            t = setTimeout(function () {
+                self.className = 'NameHighlights';
+            }, 300);
+        };
+    })();
+}
+
+function hideAll() {
+    for (var i = span.length; i--;) {
+        span[i].className = 'NameHighlights'; 
+    }
+};
